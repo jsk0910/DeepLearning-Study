8f77b29b84956a508f94e3146cc11022d823722e
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-05 13:32:19 +0900

    update resnet

--- ResNet/resnet.py
+++ ResNet/resnet.py
@@ -4,6 +4,8 @@ import torch.optim as optim
 import torch.nn.init as init
 import torch.nn.functional as F
 
+# Original ResNet
+
 class BasicBlock(nn.Module):
     mul = 1
 
@@ -41,8 +43,89 @@ class BottleNeck(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(BottleNeck, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
 
-class ResNet:
-    def __init__(self):
-        pass
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.conv3 = nn.Conv2d(out_channels, out_channels*self.mul, kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels*self.mul)
+
+        self.short_cut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels*self.mul:
+            self.short_cut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels*self.mul, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels*self.mul)
+            )
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out += self.short_cut(x)
+        out = F.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = nn.Conv2d(3, self.in_channels, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(self.in_channels)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+
+        self.linear = nn.Linear(512 * block.mul, num_classes)
+
+    def make_layer(self, block, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks-1)
+        layers = []
+        for i in range(num_blocks):
+            layers.append(block(self.in_channels, out_channels, strides[i]))
+            self.in_channels = block.mul * out_channels
+
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.maxpool1(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.linear(out)
+
+        return out
+    
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
+
+def ResNet34():
+    return ResNet(BasicBlock, [3, 4, 6, 3])
+
+def ResNet50():
+    return ResNet(BottleNeck, [3, 4, 6, 3])
+
+def ResNet101():
+    return ResNet(BottleNeck, [3, 4, 23, 3])
+
+def ResNet152():
+    return ResNet(BottleNeck, [3, 8, 36, 3])
