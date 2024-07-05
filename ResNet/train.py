4680db64b7b845269e7e130c2377bf047fe21932
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-05 13:33:21 +0900

    update training code

--- ResNet/train.py
+++ ResNet/train.py
@@ -16,3 +16,20 @@ batch_size = 50
 learning_rate = 0.0002
 num_epoch = 100
 
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# define dataset
+cifar10_train = datasets.CIFAR10(root="../Data/", train=True, transform=transform, target_transform=None, download=True)
+cifar10_test = datasets.CIFAR10(root="../Data/", train=False, transform=transform, target_transform=None, download=True)
+
+# define loader
+train_loader = DataLoader(cifar10_train,batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
+test_loader = DataLoader(cifar10_test,batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
+
+# define classes
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+device = torch.device("cpu")
+model = ResNet(base_dim=64).to(device)
+loss_func = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(),lr=learning_rate)
