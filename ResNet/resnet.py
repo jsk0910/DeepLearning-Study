8e6f1866639e6221029f1fd5c6ca7e0bd9af51ba
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-05 13:03:08 +0900

    make requirements

--- ResNet/resnet.py
+++ ResNet/resnet.py
@@ -1,3 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.init as init
+
 class ResNet:
     def __init__(self):
         pass
---
+++ ResNet/train.py
@@ -0,0 +1,16 @@
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tqdm
+from tqdm.auto import trange
+
+batch_size = 50
+learning_rate = 0.0002
+num_epoch = 100
+
