33fc7301b4a4901526eb575c6d5203da9d46a826
SangKyun Jeon <sangkyun.jeon@gmail.com>
2024-07-05 14:04:26 +0900

    update train & test

--- ResNet/train.py
+++ ResNet/train.py
@@ -12,40 +12,38 @@ from tqdm.auto import trange
 
 from resnet import *
 
-batch_size = 50
-learning_rate = 0.0002
-num_epoch = 100
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def resnet_train_cifar10(batch_size=50, learning_rate=0.0002, num_epoch=100):
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-# define dataset
-cifar10_train = datasets.CIFAR10(root="./Data/", train=True, transform=transform, target_transform=None, download=True)
-cifar10_test = datasets.CIFAR10(root="./Data/", train=False, transform=transform, target_transform=None, download=True)
+    # define dataset
+    cifar10_train = datasets.CIFAR10(root="./Data/", train=True, transform=transform, target_transform=None, download=True)
+    cifar10_test = datasets.CIFAR10(root="./Data/", train=False, transform=transform, target_transform=None, download=True)
 
-# define loader
-train_loader = DataLoader(cifar10_train,batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
-test_loader = DataLoader(cifar10_test,batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
+    # define loader
+    train_loader = DataLoader(cifar10_train,batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
+    test_loader = DataLoader(cifar10_test,batch_size=batch_size, shuffle=False, num_workers=2, drop_last=True)
 
-# define classes
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    # define classes
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-device = torch.device("cpu")
-model = ResNet50().to(device)
-loss_func = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    device = torch.device("cpu")
+    model = ResNet50().to(device)
+    loss_func = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-loss_arr = []
-for i in trange(num_epoch):
-    for j,[image,label] in enumerate(train_loader):
-        x = image.to(device)
-        y_= label.to(device)
-        
-        optimizer.zero_grad()
-        output = model.forward(x)
-        loss = loss_func(output,y_)
-        loss.backward()
-        optimizer.step()
+    loss_arr = []
+    for i in trange(num_epoch):
+        for j,[image,label] in enumerate(train_loader):
+            x = image.to(device)
+            y_= label.to(device)
+            
+            optimizer.zero_grad()
+            output = model.forward(x)
+            loss = loss_func(output,y_)
+            loss.backward()
+            optimizer.step()
 
-    if i % 10 ==0:
-        print(loss)
-        loss_arr.append(loss.cpu().detach().numpy())
+        if i % 10 ==0:
+            print(loss)
+            loss_arr.append(loss.cpu().detach().numpy())
